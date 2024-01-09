d51ebc21aaa896defea7a637e66f349ec6b6962c
mutic <cansel.muti@commencis.com>
2024-01-09 22:44:31 +0300

    feat: add start screen beginning snapshot

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,29 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter with Muti!!!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 232, 252),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz!'),
+          ),
+        ],
+      ),
+    );
   }
 }
