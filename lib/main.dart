a7d5b2a2176de36058e36f973ca128951db0980a
mutic <cansel.muti@commencis.com>
2024-01-09 22:10:20 +0300

    feat: first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_game/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
