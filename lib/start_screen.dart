400f937af2b54b4156628cc97bfefd6bd8204df1
mutic <cansel.muti@commencis.com>
2024-01-10 10:28:14 +0300

    feat: add transparency to the Widget

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,7 +9,11 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(100, 255, 255, 255),
+          ),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter with Muti!!!',
