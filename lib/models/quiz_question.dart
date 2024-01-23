65b3cff3399b532637b7b4fb2189a5e46f211ddd
muticansel <cancel.muti@gmail.com>
2024-01-23 23:00:50 +0300

    feat: use shuffle to reorder the answers

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,6 +2,12 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+
   const QuizQuestion(
     this.text,
     this.answers,
