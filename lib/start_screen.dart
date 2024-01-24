d170586975e49c418af701c324d45dd5ed188f06
muticansel <cancel.muti@gmail.com>
2024-01-24 11:55:28 +0300

    chore: add some styling

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -17,11 +18,12 @@ class StartScreen extends StatelessWidget {
             color: const Color.fromARGB(100, 255, 255, 255),
           ),
           const SizedBox(height: 80),
-          const Text(
+          Text(
             'Learn Flutter with Muti!!!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 232, 252),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 200, 131, 234),
               fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 30),
