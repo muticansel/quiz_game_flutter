d5bda085bf5a435124df29cd38e99b6a5487f911
muticansel <cancel.muti@gmail.com>
2024-01-30 13:09:27 +0300

    feat: add summary data for results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_game/data/questions.dart';
+
 class ResultScreen extends StatelessWidget {
   const ResultScreen({
     super.key,
@@ -7,6 +9,21 @@ class ResultScreen extends StatelessWidget {
   });
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i],
+        'correct_answer': questions[i].answers[0],
+        'uesr_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
